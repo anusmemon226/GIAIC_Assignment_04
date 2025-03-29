b008c5bf8e3836231a966af70e258c2c3187e067
anasmemon226 <anusm226@gmail.com>
2025-03-29 05:46:00 +0500

    Fifth Commit

---
+++ assignment_00_to_05/06_functions/01_chaotic_counting.py
@@ -0,0 +1,20 @@
+import random
+def chaotic_counting():
+    for i in range(10):
+        curr_num = i + 1
+        if done():
+            return
+        print(curr_num)
+
+def done():
+    if random.random() < 0.25:
+        return True
+    return False
+
+def main():
+    print("I'm going to count until 10 or until I feel like stopping, whichever comes first.")
+    chaotic_counting()
+    print("I'm done")
+
+if __name__ == "__main__":
+    main()
